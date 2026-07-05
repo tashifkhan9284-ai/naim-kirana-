ed52dc8e633ae1f1686df731d69fcff07c53694e
tashifkhan9284-ai <tashifkhan9284@gmail.com>
2026-07-05 18:14:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -118,4 +118,14 @@ function orderNow(productName) {
   const url = `https://wa.me/${phone}?text=${encodeURIComponent(message)}`;
 
   window.open(url, "_blank");
-}
+}let currentLang = "en";
+
+langButtons.forEach(btn => {
+  btn.addEventListener("click", () => {
+    langButtons.forEach(b => b.classList.remove("active"));
+    btn.classList.add("active");
+
+    currentLang = btn.dataset.lang;
+    displayProducts(products);
+  });
+});
